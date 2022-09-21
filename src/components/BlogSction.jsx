48f83edeeb4abe1a87dbcaa8b2f3ebcc6f18e46d
Attari Ak <akaabdullah00@gmail.com>
2022-09-22 01:40:57 +0500

    arrayAdded in Job posting

--- src/components/BlogSction.jsx
+++ src/components/BlogSction.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const BlogSction = () => {
+  const alljobs = [
+    {
+      id: 1,
+      title: 'GAME DEVELOPER',
+    },
+    {
+      id: 2,
+      title: 'SENIOR 3D ARTIST',
+    },
+    {
+      id: 3,
+      title: 'GAME DESIGNER',
+    },
+  ]
+  const [jobTitle, setJobTitle] = useState('')
+
   return (
     <>
       <section className='section blog' id='carrier' aria-label='blog'>
@@ -14,24 +30,14 @@ const BlogSction = () => {
             Following are the new openings to join our Team
           </p>
           <div className='carrier-section'>
-            <div className='carrier-card'>
-              <h2 className='carrier-title'>GAME DEVELOPER</h2>
-              <button className='apply-btn'>
-                <Link to='/jobs'>Apply now</Link>
-              </button>
-            </div>
-            <div className='carrier-card'>
-              <h2 className='carrier-title'>SENIOR 3D ARTIST</h2>
-              <button className='apply-btn'>
-                <Link to='/jobs'>Apply now</Link>
-              </button>
-            </div>
-            <div className='carrier-card'>
-              <h2 className='carrier-title'>GAME DESIGNER</h2>
-              <button className='apply-btn'>
-                <Link to='/jobs'>Apply now</Link>
-              </button>
-            </div>
+            {alljobs.map((job) => (
+              <div className='carrier-card'>
+                <h2 className='carrier-title'>{job.title}</h2>
+                <button className='apply-btn'>
+                  <Link to='/jobs'>Apply now</Link>
+                </button>
+              </div>
+            ))}
           </div>
         </div>
       </section>
